1e0e14e7095394cbcd45bcac7756f897689256bf
DashWisher <andrewryanonline@gmail.com>
2025-06-01 12:44:02 +0300

    Setting up the linter

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function about() {
+  useEffect(() => 0,[]);
+  
   return (
     <div>
       <h1>This is about page</h1>
